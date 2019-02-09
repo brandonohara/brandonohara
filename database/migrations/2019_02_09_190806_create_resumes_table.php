a51b7aab409a5c86b23f3c6df21b2aad211abf45
Brandon O'Hara <brandon@brandonohara.com>
2019-02-09 12:40:35 -0700

    Create resume model to store resume list items

---
+++ database/migrations/2019_02_09_190806_create_resumes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResumesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resumes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('group');
+            $table->string('title');
+            $table->string('description');
+            $table->string('skills')->nullable();
+            $table->string('location')->nullable();
+            $table->string('date')->nullable();
+            $table->string('url')->nullable();
+            $table->unsignedInteger('order');
+            $table->boolean('visible')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resumes');
+    }
+}
