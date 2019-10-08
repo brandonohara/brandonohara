664b36fc21ce763bf73e5ea2e3bc6fed0ed82454
Brandon O'Hara <brandon@brandonohara.com>
2019-10-08 09:45:12 -0600

    Update navigation to use Bootstrap 4

--- resources/views/navigation/topnavigation.blade.php
+++ resources/views/navigation/topnavigation.blade.php
@@ -1,29 +1,29 @@
-<header class="header" id="header">
-    <nav class="navbar navbar-expand-lg navbar-custom fixed-top" role="navigation">
-        <div class="container">
-            <a class="navbar-brand" href="{{ route('home') }}">Brandon OHara</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="custom-collapse">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a href="{{ route('home') }}#development">Development</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('home') }}#plugins">Plugins</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('home') }}#resume">Resume</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('home') }}#about">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('home') }}#contact">Contact</a>
-                    </li>
-                </ul>
-            </div>
+<nav class="navbar navbar-expand-lg fixed-top" role="navigation">
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('home') }}">Brandon OHara</a>
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#custom-collapse" aria-controls="custom-collapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="icon-bar top-bar"></span>
+            <span class="icon-bar middle-bar"></span>
+            <span class="icon-bar bottom-bar"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="custom-collapse">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="{{ route('home') }}#development" data-target="#development" class="nav-link scroll-to">Development</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('home') }}#plugins" data-target="#plugins" class="nav-link scroll-to">Plugins</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('home') }}#resume" data-target="#resume" class="nav-link scroll-to">Resume</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('home') }}#about" data-target="#about" class="nav-link scroll-to">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('home') }}#contact" data-target="#contact" class="nav-link scroll-to">Contact</a>
+                </li>
+            </ul>
         </div>
-    </nav>
-</header>
+    </div>
+</nav>
