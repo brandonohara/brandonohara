b5f4a10e7b3c7f877ddb266a2ebe5920c4542211
Brandon O'Hara <brandon@brandonohara.com>
2019-10-15 10:00:06 -0600

    Update skills to include Statamic

--- resources/lang/en/skills.php
+++ resources/lang/en/skills.php
@@ -5,6 +5,6 @@ return [
     'frontend' => ' Vue.JS, AngularJS, Tailwind, Javascript, Sass, jQuery, Bootstrap',
     'backend' => 'PHP, Laravel, Codeigniter, PHPUnit, MySQL, PostgreSQL, Redis',
     'tools' => 'Webpack, Vagrant, Gulp, Git, NPM, Laravel Valet, Envoyer, Composer',
-    'cms' => 'ExpressionEngine, Craft CMS, Drupal 8, Magento, Wordpress, Photoshop'
+    'cms' => 'Statamic, ExpressionEngine, Craft CMS, Drupal 8, Wordpress, Photoshop'
 
 ];
