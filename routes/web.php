d6cb9fe8054bff47dcc5f9d3d7f85d5284ed630c
Brandon O'Hara <brandon@brandonohara.com>
2020-04-06 19:00:23 -0600

    Example HTML for Ryan to learn

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ Auth::routes(['register' => false, 'verify' => false]);
 Route::get('/', 'HomeController@index')->name('home');
 Route::post('/contact', 'ContactController@store')
         ->name('contact.store')
-        ->middleware(\Spatie\Honeypot\ProtectAgainstSpam::class);;
+        ->middleware(\Spatie\Honeypot\ProtectAgainstSpam::class);
